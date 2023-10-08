2974b389aa4c440835faff26f85748c9f0350b32
Sudo-Self <sudo-self@proton.me>
2023-10-07 20:33:07 -0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,7 @@
           <icon name="ph:paper-plane-tilt-bold" class="h-4 w-4" />
         </button>
         <a
-          href="https://github.com/sudo-self/onelink"
+          href="https://github.com/sudo-self/vue-js-link-in-bio"
           target="_blank"
           class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium bg-white text-slate-700"
         >
